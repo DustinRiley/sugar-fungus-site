29eb417ad9dd1a852c84336c549a939e4b1aa2f3
dustinriley <dustin.riley@me.com>
2022-04-03 16:27:06 -0700

    mouse hover effect

--- src/components/GlitchImage/GlitchImage.jsx
+++ src/components/GlitchImage/GlitchImage.jsx
@@ -1,22 +1,44 @@
-import { useContext, createContext, useState } from "react";
+import { useContext, createContext, useState, useCallback } from "react";
 import { useEffect, useRef } from "react";
 import { EffectCanvas } from "../../three/glitch";
 
 const sceneContext = createContext();
 
-export function GlitchImageCard({ imgSrc, text }) {
+export function GlitchImageCard({ imgSrc, text, textLocation, textColor }) {
   const { effectCanvas } = useContext(sceneContext);
+  const [meshItem, setMeshItem] = useState()
   console.log(imgSrc)
   const imgRef = useRef(null);
   useEffect(() => {
     if (!imgRef.current || !effectCanvas) return;
-    effectCanvas.createMeshItems([imgRef.current]);
+    const meshItemClass = effectCanvas.createMeshItem(imgRef.current);
+    setMeshItem(meshItemClass)
   }, [imgRef, effectCanvas]);
+
+  const handleMouseMove = useCallback((e)=> {
+    if(!meshItem) return;
+    console.log({x: e.movementX, y: e.movementY});
+    meshItem.setMouseXY(e.movementX, e.movementY);
+    meshItem.setMouseRenderFunction(true);
+  },[meshItem])
+
+
+  const handleMouseOver = useCallback((e)=> {
+    console.log('here', e);
+    if(!meshItem) return;
+    meshItem.setMouseRenderFunction(true);
+  },[meshItem])
+
+
+  const handleMouseOut = useCallback((e)=> {
+    if(!meshItem) return;
+    meshItem.setMouseRenderFunction(false);
+  },[meshItem])
 
   return (
     <div className="image-container">
       <h1>{text}</h1>
-      <img ref={imgRef} src={imgSrc} alt="" />
+      <img onMouseOver={handleMouseOver} onMouseLeave={handleMouseOut} onMouseMove={handleMouseMove} ref={imgRef} src={imgSrc} alt="" />
     </div>
   );
 }
